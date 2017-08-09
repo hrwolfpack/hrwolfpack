4d43b54b0884f72efbc13dac067421b3e8c9cd2f
jsonlitz <jasondotlee@gmail.com>
2017-08-09 16:37:08 -0700

    refactor hide and show modals, delete modal.jsx

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Listings from './Listings.jsx'
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
 
 class Dashboard extends React.Component {
+
   constructor(props){
     super(props);
     this.state ={
+      lgShow: false
     };
   }
-  handleClick(e){
-    e.preventDefault();
-    console.log('the create was clicked');
+
+  hideModal(e){
+    this.setState({
+      lgShow: false
+    });
   }
+
+  showModal(e){
+    e.preventDefault();
+    this.setState({
+      lgShow: true
+    });
+  }
+
 
   render () {
     return (
       <div>
+        <Modal show={this.state.lgShow}  bsSize="large" aria-labelledby="contained-modal-title-sm">
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-sm">Create New Listing</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Sup</h4>
+            <p>Some awesome form here</p>
+            
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="danger" onClick={this.hideModal.bind(this)}>Cancel</Button>
+            <Button bsStyle="success" onClick={this.showModal.bind(this)}>Create</Button>
+
+          </Modal.Footer>
+        </Modal>
         <Listings/>
-        <Button bsStyle="primary" onClick = {this.handleClick.bind(this)}>'Create.jsx'</Button>
+        <Button bsStyle="primary" onClick={this.showModal.bind(this)}>Create Listing</Button>
       </div>
     )
   }
