cad1118a16d9912a56adeafaf3bc71b853bc7511
Kevin Huang <kevinhuang@Kevins-MacBook-Pro.local>
2017-08-08 23:12:22 -0700

    Heroku Pipeline test

--- server/index.js
+++ server/index.js
@@ -9,6 +9,6 @@ app.get('/test', (req, res) => {
   res.send('hello world');
 });
 
-let port = process.env.PORT;
-// export PORT = 3000;
+let port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log('Listening on port ', port));
