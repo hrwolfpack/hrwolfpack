c29cb04cab57cf1f756404b87d236540712a5d1b
jsonlitz <jasondotlee@gmail.com>
2017-08-09 23:00:23 -0700

    add post request submit handler in ListingsForm.jsx

--- client/src/components/ListingsForm.jsx
+++ client/src/components/ListingsForm.jsx
@@ -1,51 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { FormGroup, InputGroup, FormControl, Button, DropdownButton, MenuItem } from 'react-bootstrap';
+import $ from 'jquery';
 
-function Form (props){
-  return (
-    <form>
+class Form extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      price: "",
+      location: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-      <FormGroup>
-        <InputGroup>
-          <FormControl type="text" placeholder = "Item Name"/>
-          <InputGroup.Addon></InputGroup.Addon>
-        </InputGroup>
-      </FormGroup>
+  onChange(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-      <FormGroup>
-        <InputGroup>
-          <InputGroup.Addon>$</InputGroup.Addon>
-          <FormControl type="text" placeholder = "Cost"/>
-          <InputGroup.Addon></InputGroup.Addon>
-        </InputGroup>
-      </FormGroup>
+  onSubmit(e){
+    console.log(this);
+    var that = this;
+    e.preventDefault();
+    $.post("/listings", {
+      name: that.state.name, price: that.state.price, location: that.state.location}, function(data){
+    });
+  }
 
-      <FormGroup>
-        <InputGroup>
-          <FormControl type="text" placeholder = "Pickup Location"/>
-          <InputGroup.Addon></InputGroup.Addon>
-        </InputGroup>
-      </FormGroup>
+  render () {
+    return (
+      <form>
 
-          <DropdownButton
-            componentClass={InputGroup.Button}
-            id="input-dropdown-addon"
-            title="Pack Size"
-            >
-            <MenuItem key="1">2</MenuItem>
-            <MenuItem key="2">3</MenuItem>
-            <MenuItem key="3">4</MenuItem>
-            <MenuItem key="4">5</MenuItem>
-            <MenuItem key="5">6</MenuItem>
-            <MenuItem key="6">7</MenuItem>
-            <MenuItem key="7">8</MenuItem>
-          </DropdownButton>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Item Name"
+              name="name"
+              value={this.state.name}
+              onChange={this.onChange}
+              />
+            <InputGroup.Addon></InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
 
-    </form>
+        <FormGroup>
+          <InputGroup>
+            <InputGroup.Addon>$</InputGroup.Addon>
+            <FormControl
+              type="text"
+              placeholder="Price"
+              name="price"
+              value={this.state.price}
+              onChange={this.onChange}
+              />
 
-  );
+            <InputGroup.Addon></InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
 
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder = "Pickup Location"
+              name="location"
+              value={this.state.location}
+              onChange={this.onChange}
+              />
+            <InputGroup.Addon></InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        <DropdownButton
+          componentClass={InputGroup.Button}
+          id="input-dropdown-addon"
+          title="Pack Size"
+          >
+          <MenuItem key="1">2</MenuItem>
+          <MenuItem key="2">3</MenuItem>
+          <MenuItem key="3">4</MenuItem>
+          <MenuItem key="4">5</MenuItem>
+          <MenuItem key="5">6</MenuItem>
+          <MenuItem key="6">7</MenuItem>
+          <MenuItem key="7">8</MenuItem>
+        </DropdownButton>
+        <Button bsStyle="success" onClick={this.onSubmit}>Create</Button>
+
+      </form>
+    );
+  }
 }
 
 export default Form;
