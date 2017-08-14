8b109d01223e23e1a7a6e40f74432f663b106ecd
Kevin Huang <kevinhuang@Kevins-MacBook-Pro.local>
2017-08-14 12:35:27 -0700

    Reorganize routes using routes.js with express Router module

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 const passport = require('passport');
-const db = require('../db');
+// const db = require('../db');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const passportConfig = require('./passport.js');
+const router = require('./routes.js');
 
 let app = express();
 //Use middleware
@@ -30,9 +31,14 @@ var isLoggedIn = (req, res, next) => {
 };
 app.get('/', isLoggedIn);
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use('/login', express.static(path.join(__dirname, '../client/dist/login.html')));
-app.use('/signup', express.static(path.join(__dirname, '../client/dist/signup.html')));
 
+app.get('/login', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/dist/login.html'));
+});
+
+app.get('/signup', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/dist/signup.html'));
+});
 
 app.post('/login',
 	passport.authenticate('local-login', {
@@ -51,110 +57,7 @@ app.get('/logout', (req, res) => {
   res.redirect('/login');
 });
 
-app.post('/listings', (req,res) => {
-	db.Listing.create({name: req.body.name, price: parseInt(req.body.price), location: req.body.location, initializer: req.user.id })
-	.then(listing => {
-		res.send('success');
-	});
-})
-
-app.get('/listings', (req, res) => {
-	db.Listing.findAll()
-		.then(results => {
-			res.send(results);
-		})
-		.catch(err => {
-			console.log('Error: ', err);
-		});
-})
-
-app.get('/user', (req, res) => {
-	var {id, username} = req.user;
-	res.send({id, username});
-});
-
-app.post('/join', (req, res) => {
-	db.UserListings.create(
-		{listing_id: req.body.listingId, 
-			user_id: req.user.id})
-		.then(results => {
-			res.send(results);
-		})
-		.catch(err => {
-			console.log('Error', err);
-		});
-});
-
-app.post('/userListings', (req, res) => {
-	db.UserListings.findAll({
-		where: {
-			listing_id: req.body.listingId,
-			user_id: req.user.id
-		}
-	})
-	.then(results => {
-		res.send(results);
-	})
-	.catch(err => {
-		console.log('Error: ', err);
-	})
-});
-
-app.post('/packsize', (req, res) => {
-	db.UserListings.findAndCountAll({
-		where: {listing_id: req.body.listingId}
-	})
-	.then(results => {
-		res.send(results);
-	})
-	.catch(err => {
-		console.log('Error: ', err);
-	});
-});
-
-app.post('/arrived', (req, res) => {
-	db.Listing.update({
-		arrived: true
-	}, {
-		where: {id: req.body.listingId}
-	})
-	.then(result => {
-		res.send('Listing status updated');
-	})
-	.catch(err => {
-		console.log('Error: ', err);
-	});
-});
-
-app.post('/received', (req, res) => {
-	db.UserListings.update({
-		received: true
-	}, {
-		where: {
-			listing_id: req.body.listingId,
-			user_id: req.user.id
-		}
-	})
-	.then(result => {
-		res.send('Received!');
-	})
-	.catch(err => {
-		console.log('Error: ', err);
-	});
-});
-
-app.post('/receiveCount', (req, res) => {
-	db.UserListings.findAndCountAll({
-		where: {listing_id: req.body.listingId,
-				received: true}
-	})
-	.then(result => {
-		res.send(result);
-	})
-	.catch(err => {
-		console.log('Error: ', err);
-	});
-});
+app.use(router);
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening on port ', port));
---
+++ server/routes.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.post('/listings', (req,res) => {
+	db.Listing.create({name: req.body.name, price: parseInt(req.body.price), location: req.body.location, initializer: req.user.id })
+	.then(listing => {
+		res.send('success');
+	});
+})
+
+router.get('/listings', (req, res) => {
+	db.Listing.findAll()
+		.then(results => {
+			res.send(results);
+		})
+		.catch(err => {
+			console.log('Error: ', err);
+		});
+})
+
+router.get('/user', (req, res) => {
+	var {id, username} = req.user;
+	res.send({id, username});
+});
+
+router.post('/join', (req, res) => {
+	db.UserListings.create(
+		{listing_id: req.body.listingId, 
+			user_id: req.user.id})
+		.then(results => {
+			res.send(results);
+		})
+		.catch(err => {
+			console.log('Error', err);
+		});
+});
+
+router.post('/userListings', (req, res) => {
+	db.UserListings.findAll({
+		where: {
+			listing_id: req.body.listingId,
+			user_id: req.user.id
+		}
+	})
+	.then(results => {
+		res.send(results);
+	})
+	.catch(err => {
+		console.log('Error: ', err);
+	})
+});
+
+router.post('/packsize', (req, res) => {
+	db.UserListings.findAndCountAll({
+		where: {listing_id: req.body.listingId}
+	})
+	.then(results => {
+		res.send(results);
+	})
+	.catch(err => {
+		console.log('Error: ', err);
+	});
+});
+
+router.post('/arrived', (req, res) => {
+	db.Listing.update({
+		arrived: true
+	}, {
+		where: {id: req.body.listingId}
+	})
+	.then(result => {
+		res.send('Listing status updated');
+	})
+	.catch(err => {
+		console.log('Error: ', err);
+	});
+});
+
+router.post('/received', (req, res) => {
+	db.UserListings.update({
+		received: true
+	}, {
+		where: {
+			listing_id: req.body.listingId,
+			user_id: req.user.id
+		}
+	})
+	.then(result => {
+		res.send('Received!');
+	})
+	.catch(err => {
+		console.log('Error: ', err);
+	});
+});
+
+router.post('/receiveCount', (req, res) => {
+	db.UserListings.findAndCountAll({
+		where: {listing_id: req.body.listingId,
+				received: true}
+	})
+	.then(result => {
+		res.send(result);
+	})
+	.catch(err => {
+		console.log('Error: ', err);
+	});
+});
+
+module.exports = router;
