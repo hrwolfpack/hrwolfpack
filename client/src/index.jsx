4a018fe23c4e53a4e93faccb3e3ecf54b3eb7121
Kevin Huang <kevinhuang@Kevins-MacBook-Pro.local>
2017-08-17 17:32:15 -0700

    Start using React Router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './components/Dashboard.jsx';
 import $ from 'jquery';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import io from 'socket.io-client';
 let socket = io('http://localhost:3000');
 
@@ -29,10 +31,51 @@ class App extends React.Component {
         <div>
           Hello, {this.state.currentUser}
         </div>
-        <Dashboard userId={this.state.userId} socket={socket}/>
+
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href='#'>WolfPack</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem>
+              <Link to="/new">New Listings Near You</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/joined">Listings You Joined</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/initiated">Listings You Initiated</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/dashboard">Dashboard</Link>
+            </NavItem>
+          </Nav>
+        </Navbar> 
+
+        <Route path="/new" render={(props) => (
+          <div>Here are all the new listings!</div>
+        )}/>
+        <Route path="/joined" render={(props) => (
+          <div>Here are all the listings you have joined!</div>
+        )}/>
+        <Route path="/initiated" render={(props) => (
+          <div>Here are all the listings you have initiated!</div>
+        )}/>
+        <Route path="/dashboard" render={(props) => (
+          <Dashboard userId={this.state.userId} socket={socket}/>
+        )}/>
+
+
       </div>
     );
   }
+        // <Dashboard userId={this.state.userId} socket={socket}/>
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('app'));
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const db = require('../db');
 
 router.get('/listings', (req, res) => {
@@ -27,6 +28,10 @@ router.post('/listingStatus', (req, res) => {
 	.catch(err => {
 		console.log('Error: ', err);
 	});
+});
+
+router.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 module.exports = router;
