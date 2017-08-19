f7c8a8ca0d6f64ca846752a93ea729be706153d8
Kevin Huang <kevinhuang@Kevins-MacBook-Pro.local>
2017-08-18 22:27:24 -0700

    Add Start Campaign button

--- client/src/components/Deals.jsx
+++ client/src/components/Deals.jsx
@@ -4,7 +4,7 @@ import Deal from './Deal.jsx';
 var Deals = (props) => (
 	<div>
 		{props.dealInfos.map((dealInfo, i) => {
-			return <Deal dealInfo={dealInfo} key={i}/> ;
+			return <Deal dealInfo={dealInfo} handleSelect={props.handleSelect} key={i}/> ;
 		})}
 	</div>
 );
