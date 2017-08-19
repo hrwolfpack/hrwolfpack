a5d1c6426ae02d02859f3887e792db09528856f7
jsonlitz <jasondotlee@gmail.com>
2017-08-19 13:44:43 -0700

    change navBar style

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,35 +1,72 @@
 import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 var Header = (props) => (
 	<div>
 
-	    <Navbar>
-	      <Navbar.Header>
-	        <Navbar.Brand>
 
-							<div>
-					      Hello, {props.currentUser}
-					    </div>
-	        </Navbar.Brand>
-	      </Navbar.Header>
-	      <Nav>
-	        <NavItem>
-	          <Link to="/">Explore</Link>
-	        </NavItem>
-	        <NavItem>
-	          <Link to="/new">New Listings Near You</Link>
-	        </NavItem>
-	        <NavItem>
-	          <Link to="/joined">Listings You Joined</Link>
-	        </NavItem>
-	        <NavItem>
-	          <Link to="/initiated">Listings You Initiated</Link>
-	        </NavItem>
-	      </Nav>
-	    </Navbar>
+
+			<Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">
+					Hello, {props.currentUser}
+				</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#">
+					<Link to="/joined">Listings You Joined</Link>
+        </NavItem>
+
+        <NavItem eventKey={2} href="#">
+					<Link to="/joined">Listings You Joined</Link>
+        </NavItem>
+
+				<NavItem eventKey={2} href="#">
+					<Link to="/initiated">Listings You Initiated</Link>
+        </NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 	</div>
 );
 
 export default Header;
+
+// <Navbar>
+// 	<Navbar.Header>
+// 		<Navbar.Brand>
+//
+//
+// 		</Navbar.Brand>
+// 	</Navbar.Header>
+// 	<Nav>
+// 		<NavItem>
+// 			<Link to="/">Explore</Link>
+// 		</NavItem>
+// 		<NavItem>
+// 			<Link to="/new">New Listings Near You</Link>
+// 		</NavItem>
+// 		<NavItem>
+// 			<Link to="/joined">Listings You Joined</Link>
+// 		</NavItem>
+// 		<NavItem>
+// 			<Link to="/initiated">Listings You Initiated</Link>
+// 		</NavItem>
+// 	</Nav>
+// </Navbar>
