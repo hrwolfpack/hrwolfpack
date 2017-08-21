2d1832584787ee6efe02c9c0501c2b217fcbc38a
Kevin Huang <kevinhuang@Kevins-MacBook-Pro.local>
2017-08-21 12:02:15 -0700

    Use Heroku PG database

--- db/index.js
+++ db/index.js
@@ -1,20 +1,31 @@
 var Sequelize = require('sequelize');
 var cred = require('../config');
+var pg = require('pg');
+pg.defaults.ssl = true;
+
 //create 'wolfpack' database
-var db_name = cred.db_name || 'wolfpack';
-var db_username = cred.db_username || 'root';
-var db_password = cred.db_password || '';
-var db_host = cred.db_host || 'localhost';
+// var db_name = cred.db_name || 'wolfpack';
+// var db_username = cred.db_username || 'root';
+// var db_password = cred.db_password || '';
+// var db_host = cred.db_host || 'localhost';
 
-var db = new Sequelize(db_name, db_username, db_password, {
-	host: db_host,
-	dialect: 'mysql',
-	logging: false,
-	define: {
-        timestamps: false
-    }
-});
+// var db = new Sequelize(db_name, db_username, db_password, {
+// 	host: db_host,
+// 	dialect: 'mysql',
+// 	logging: false,
+// 	define: {
+//         timestamps: false
+//     }
+// });
 
+var db_url = process.env.DATABASE_URL || 'postgres://svntauetywrtss:55f43a82d494c48a4e4fa78280e969f4ae787e010d673b173ac1b9c8edced9e0@ec2-184-73-249-56.compute-1.amazonaws.com:5432/dbnte43httuu78';
+var db = new Sequelize(db_url);
+
+//staging
+// var db = new Sequelize('postgres://svntauetywrtss:55f43a82d494c48a4e4fa78280e969f4ae787e010d673b173ac1b9c8edced9e0@ec2-184-73-249-56.compute-1.amazonaws.com:5432/dbnte43httuu78');
+
+//production
+// var db = new Sequelize('postgres://vdzxhcdefalggv:92cc10539f76f333d3a52a5a47227177fd05635db543e25e180557c05ed97c00@ec2-50-19-219-69.compute-1.amazonaws.com:5432/dbsp7ma4q35gml');
 
 //test connection
 db
@@ -36,16 +47,16 @@ var User = db.define('user', {
 
 var Listing = db.define('listing', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-  name: Sequelize.STRING,
-	description: Sequelize.STRING,
-	image_url: Sequelize.STRING,
-  url: Sequelize.STRING,
+  name: Sequelize.TEXT,
+	description: Sequelize.TEXT,
+	image_url: Sequelize.TEXT,
+  url: Sequelize.TEXT,
   initializer: { type: Sequelize.INTEGER, defaultValue: 1},
   price: Sequelize.DECIMAL(5, 2),
   completed: { type: Sequelize.BOOLEAN, defaultValue: false},
   arrived: { type: Sequelize.BOOLEAN, defaultValue: false},
   packed: { type: Sequelize.BOOLEAN, defaultValue: false},
-  location: Sequelize.STRING,
+  location: Sequelize.TEXT,
 	lat: { type: Sequelize.DECIMAL(10, 8), defaultValue: 0 },
 	lng: { type: Sequelize.DECIMAL(11, 8), defaultValue: 0 },
   num_of_participants:{ type: Sequelize.INTEGER, defaultValue: 4},
@@ -60,13 +71,23 @@ User.belongsToMany(Listing, { through: UserListings});
 Listing.belongsToMany(User, { through: UserListings});
 //create Users and listings table
 
-UserListings.sync()
+User.sync()
 .then(() => {
   return Listing.sync();
 })
 .then(() => {
-  return User.sync();
-})
+  return UserListings.sync();
+});
+
+
+
+// UserListings.sync()
+// .then(() => {
+//   return Listing.sync();
+// })
+// .then(() => {
+//   return User.sync();
+// })
 // .then(() => User.create({username: 'dylan', password: '123d'}))
 // .then(() => User.create({username: 'clarence', password: '123c'}))
 // .then(() => User.create({username: 'kevin', password: '123k'}))
