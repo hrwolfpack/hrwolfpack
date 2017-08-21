90b8292529b14af84f7e332c81a2031a292041eb
dylan <dylan@dylans-MacBook-Pro.local>
2017-08-21 14:14:09 -0700

    Serve new listings on successful login

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.post('/login',
 	passport.authenticate('local-login', {
-		successRedirect: '/',
+		successRedirect: '/new',
 		failureRedirect: '/login'})
 );
 
